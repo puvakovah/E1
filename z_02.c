e2e559699885fa1406f0ab6b4bbc54ddb82f1c14
puvakovah <puvakova.h@gmail.com>
2024-04-02 16:42:00 +0200

    vytvorenie procedury zjednotenie

--- z_02.c
+++ z_02.c
@@ -6,15 +6,17 @@ void vypis_mnozin(int m[], int velkost_m)
 {
      for(int i=0; i<velkost_m ; i++)
      {
-          printf("%d\t",m[i]);
-
+          printf( " %d ",m[i]);
      }
 }
+
 void prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
 {
      int P=0;
      int m[M];
-     
+
+     printf("Prienik mnozin = {");
+
      for(int i=0; i<velkost_m1 ; i++)
      {
           for(int j=0; j<velkost_m2;j++)
@@ -26,12 +28,34 @@ void prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
           }    
      }
      vypis_mnozin(m,P);
+     printf("}\n");
 }
+
+void zjednotenie(int m1[], int m2[], int velkost_m1, int velkost_m2)
+{
+     int P=0;
+     int m[M];
+
+     printf("Zjenodtenie mnozin = {");
+     
+     for(int i=0; i<velkost_m1 ; i++)
+     {
+          for(int j=0; j<velkost_m2;j++)
+          {
+          vypis_mnozin(m1,velkost_m1);
+          vypis_mnozin(m2,velkost_m2);
+          }    
+     }
+
+     printf("}\n");
+}
+
 int main()
 {
      int a[N]={1,2,3,4},b[M]={2,2,4,6,8};
      
      prienik(a,b,N,M);
+     zjednotenie(a,b,N,M);
      
      return 0;
 }
