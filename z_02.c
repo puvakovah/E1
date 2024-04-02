5cb286021c17940f5cd4a98a2998aa3f7eb0c639
puvakovah <puvakova.h@gmail.com>
2024-04-02 15:55:30 +0200

    vypisovadlo

---
+++ z_02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void vypis_mnozin(int m[], int velkost_m)
+{
+     for(int i=0; i<velkost_m ; i++)
+     {
+          printf("%d\t",m[i]);
+
+     }
+}
+int prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
+{
+     int m
+}
+int main()
+{
+     int a[4]={1,2,3,4},b[5]={2,2,4,6,8};
+     int velkost_a = sizeof(a);
+     int velkost_b = sizeof(b);
+
+}
