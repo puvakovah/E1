4d4b7e01df2f4c96dbb08d85cb78a7d26da5f424
puvakovah <puvakova.h@gmail.com>
2024-04-02 16:29:33 +0200

    prienik v main

--- z_02.c
+++ z_02.c
@@ -12,20 +12,26 @@ void vypis_mnozin(int m[], int velkost_m)
 }
 void prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
 {
+     int P=0;
+     int m[M];
+     
      for(int i=0; i<velkost_m1 ; i++)
      {
           for(int j=0; j<velkost_m2;j++)
           {
                if(m1[i] == m2[j])
                {
-                    vypis_mnozin(m1,velkost_m1);
+                    m[P++] = m2[j];
                }
-          }
+          }    
      }
+     vypis_mnozin(m,P);
 }
 int main()
 {
      int a[N]={1,2,3,4},b[M]={2,2,4,6,8};
-
+     
      prienik(a,b,N,M);
+     
+     return 0;
 }
