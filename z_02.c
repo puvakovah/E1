2fece75e6f94399aec15cf64665006d817ec9a1d
puvakovah <puvakova.h@gmail.com>
2024-04-20 20:27:32 +0200

    vypis mnozin

--- z_02.c
+++ z_02.c
@@ -6,12 +6,13 @@ void vypis_mnozin(int M[], int n)
 {
      for(int i=0; i<n ; i++)
      {
-          printf( " %d ",M[i]);
+          printf( " %d ",&M[i]);
      }
 }
 
 int* generator_mnozin(int n) {
     srand(time(NULL));
+
     int* M = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         M[i] = rand() % 101; // Generovanie od 0 po 100
@@ -61,8 +62,12 @@ int main()
     int* m1=generator_mnozin(n);
     int* m2=generator_mnozin(m);
   
+    printf("m1 = {");
     vypis_mnozin(m1,n);
-    vypis_mnozin(m2,m);
-
+    printf("}\n");
+     printf("m2 = {");
+    vypis_mnozin(m2,n);
+    printf("}\n");
+     
      return 0;
 }
