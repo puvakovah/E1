662dcb67da335a266bf90f678ae837cd3f5c5ff3
puvakovah <puvakova.h@gmail.com>
2024-04-20 21:56:01 +0200

    vypis prieniku a zjednotenia + zmena  generator_mnozin

--- z_02.c
+++ z_02.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include<time.h>
 #include<stdlib.h>
+#include <unistd.h>
 
 void vypis_mnozin(int M[], int n)
 {
      for(int i=0; i<n ; i++)
      {
-          printf( " %d ",&M[i]);
+          printf( " %d ", M[i]);
      }
 }
 
 int* generator_mnozin(int n) {
+    usleep(1000000);
     srand(time(NULL));
 
     int* M = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         M[i] = rand() % 101; // Generovanie od 0 po 100
     }
+    return M;
 }
 
 
-int prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
+int* prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
 {
      int P=0;
-     int M[P];
+     int* M = malloc((velkost_m1 + velkost_m2) * sizeof(int));
 
      for(int i=0; i<velkost_m1 ; i++)
      {
@@ -33,15 +36,15 @@ int prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
                {
                     M[P++] = m2[j];
                }
-          }    
-     }
-     
+          }      
+} 
+return M;
 }
 
-void zjednotenie(int m1[], int m2[], int velkost_m1, int velkost_m2)
+int* zjednotenie(int m1[], int m2[], int velkost_m1, int velkost_m2)
 {
      int P=0;
-     int M[P];
+     int* M = malloc((velkost_m1 + velkost_m2) * sizeof(int));
 
 
      for(int i=0; i<velkost_m1 ; i++)
@@ -53,21 +56,31 @@ void zjednotenie(int m1[], int m2[], int velkost_m1, int velkost_m2)
      {
           M[P++] = m2[i];   
      }
-
+     return M;
 }
 
 int main()
 {
      int n=6,m=8;
-    int* m1=generator_mnozin(n);
-    int* m2=generator_mnozin(m);
+     int* m1=generator_mnozin(n);
+     int* m2=generator_mnozin(m);
+     int* m_prieniku=prienik(m1,m2,n,m);
+     int* m_zjednotenia=zjednotenie(m1,m2,n,m);
   
-    printf("m1 = {");
-    vypis_mnozin(m1,n);
-    printf("}\n");
+     printf("m1 = {");
+     vypis_mnozin(m1,n);
+     printf("}\n");
+
      printf("m2 = {");
-    vypis_mnozin(m2,n);
-    printf("}\n");
+     vypis_mnozin(m2,n);
+     printf("}\n");
      
+     printf("prienik = {");
+     vypis_mnozin(m_prieniku,n+m);
+     printf("}\n");
+
+     printf("m2 = {");
+     vypis_mnozin(m_zjednotenia,n+m);
+     printf("}\n");
      return 0;
 }
