ac2783b45da375f249d40ef9cb637195e301f95b
puvakovah <puvakova.h@gmail.com>
2024-04-23 15:28:24 +0200

    presun srand(time(NULL)) do main

--- z_02.c
+++ z_02.c
@@ -11,15 +11,11 @@ void vypis_mnozin(int M[], int n)
      }
 }
 
-int* generator_mnozin(int n) {
-    usleep(1000000);
-    srand(time(NULL));
-
+int* generator_mnozin(int n, int dh, int hh) {
     int* M = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         M[i] = rand() % 101; // Generovanie od 0 po 100
     }
-
     return M;
 }
 
@@ -36,6 +32,7 @@ int* prienik(int m1[], int m2[], int velkost_m1, int velkost_m2)
                if(m1[i] == m2[j])
                {
                     M[P++] = m1[i];
+                     break;
                }
           }      
 } 
@@ -62,29 +59,27 @@ int* zjednotenie(int m1[], int m2[], int velkost_m1, int velkost_m2)
 
 int main()
 {
-     int r,n;
-     printf("Zadaj 2 velkosti pola\n");
-     scanf("%d%d",&n,&r);
-
-     int* m1=generator_mnozin(n);
-     int* m2=generator_mnozin(r);
-     int* m_prieniku=prienik(m1,m2,n,r);
-     int* m_zjednotenia=zjednotenie(m1,m2,n,r);
+     srand(time(NULL));
+     
+     int* m1=generator_mnozin(100,1,100);
+     int* m2=generator_mnozin(90,1,50);
+     int* m_prieniku=prienik(m1,m2,100,90);
+     int* m_zjednotenia=zjednotenie(m1,m2,100,90);
   
      printf("m1 = {");
-     vypis_mnozin(m1,n);
+     vypis_mnozin(m1,100);
      printf("}\n");
 
      printf("m2 = {");
-     vypis_mnozin(m2,r);
+     vypis_mnozin(m2,90);
      printf("}\n");
      
      printf("prienik = {");
-     vypis_mnozin(m_prieniku,r);
+     vypis_mnozin(m_prieniku,100);
      printf("}\n");
 
      printf("m2 = {");
-     vypis_mnozin(m_zjednotenia,n+r);
+     vypis_mnozin(m_zjednotenia,190);
      printf("}\n");
 
      free(m1);
